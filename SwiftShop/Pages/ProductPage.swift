27b782665bda02a449979d1060b953bdb9f15e20
Geordie Powers <geordie.powers@gmail.com>
2019-12-25 12:09:37 -0500

    Sheet status as enum works, needs refactor

--- SwiftShop/Pages/ProductPage.swift
+++ SwiftShop/Pages/ProductPage.swift
@@ -10,7 +10,7 @@ import SwiftUI
 import Combine
 import PartialSheet
 
-class ProductPageViewModel : ObservableObject {
+class ProductPageViewModel: ObservableObject {
   @Published var products: [SimpleProduct] = []
   @Published var tags: [Tag] = []
   private var cancellables: [AnyCancellable] = []
@@ -51,28 +51,84 @@ class ProductPageViewModel : ObservableObject {
   }
 }
 
+enum SheetStatus {
+  case closed
+  case open(_ product: SimpleProduct, isNew: Bool = false)
+  
+  func isOpen() -> Bool {
+    switch(self) {
+    case .closed:
+      return false
+    default:
+      return true
+    }
+  }
+}
+
+class EditSheetStatus: ObservableObject {
+  @Published var status: SheetStatus
+
+  init() {
+    status = .closed
+  }
+
+  func open(_ product: SimpleProduct, isNew: Bool = false) {
+    status = .open(product, isNew: isNew)
+  }
+
+  func close() {
+    status = .closed
+  }
+  
+  func isOpen() -> Bool {
+    let val = status.isOpen()
+    
+    return val
+  }
+}
+
 struct ProductPage: View {
   @ObservedObject private var model: ProductPageViewModel = ProductPageViewModel()
+  @ObservedObject private var editSheetStatus = EditSheetStatus()
   @State var editSheetOpen = false
+  // @State var editSheetStatus: EditSheetStatus = .closed
   private var editFormModel = ProductEditFormModel()
+  private var imfalse = false
+  
+  var otherstatus = SheetStatus.closed
   
   private func addProductClicked() {
     editSheetOpen = true
     editFormModel.newProduct()
+    //
+    editSheetStatus.open(SimpleProduct(id: -1, name: "", price: 0.0, tags: []), isNew: true)
   }
   
   private func productRowClicked(product: SimpleProduct) {
     editSheetOpen = true
     editFormModel.editProduct(product)
+    //
+    editSheetStatus.open(product, isNew: false)
   }
   
   private func onSaveProduct(_ product: SimpleProduct, isNew: Bool) {
     model.onSaveProduct(product, isNew)
     editSheetOpen = false
+    //
+    editSheetStatus.close()
+  }
+  
+  
+  private func testSet(_ fieldValue: Bool) {
+    self.editSheetStatus.close()
   }
   
   var body: some View {
-    VStack {
+//    let isDisplayed = Binding<Bool>(get: { self.editSheetOpen }, set: { self.editSheetOpen = true } )
+
+    let boundSearchValue = Binding<Bool>(get: self.editSheetStatus.isOpen, set: self.testSet)
+    
+    return VStack {
       HStack {
         Text("Products").font(.title)
         Spacer()
@@ -84,7 +140,7 @@ struct ProductPage: View {
       List(model.products) { p in
         ProductRow(product: p, onRowClick: self.productRowClicked)
       }
-    }.partialSheet(presented: $editSheetOpen) {
+    }.partialSheet(presented: boundSearchValue) {
       VStack {
         Text("Edit Product") // TODO: change text to 'new product' when it's new
         
